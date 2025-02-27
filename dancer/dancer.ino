e37710eafdc165e6ac21443bb983d4fe52fec14a
tomas_cakan <cakant2008@gmail.com>
2025-02-27 09:25:21 +0100

    Update dancer.ino

--- dancer/dancer.ino
+++ dancer/dancer.ino
@@ -1,8 +1,26 @@
+#include <BLEDevice.h>
 #include <ESP32Servo.h>
+
+#define bleServerName "Companero"  //name of the BLE server we are connecting to
+//UUIDs of the service and characteristics:
+static BLEUUID serviceUUID("edddc3d6-5d4a-4677-87c5-f4f7d40b6111");
+static BLEUUID musicCharUUID("90c43130-b419-4bfd-bcf4-bb9c8373ddd6");
+
+static BLEAddress* serverAddress;  //address of the device
+static BLERemoteCharacteristic* music_char;  //characteristic for music sync
+//notification options
+const uint8_t notificationOn[] = {0x1, 0x0};
+const uint8_t notificationOff[] = {0x0, 0x0};
+
+static bool serverFound = false;
+static bool connected = false;
+
+int music_command;
+bool go = 0;
 
 Servo rd, rv, lv, ld, r, l;
 
-//motors
+ //motors
 #define RR_EN 18  // Right rear enable pin
 const int RR_DIR[] = {17, 5}; // Right rear direction pins
 #define LR_EN 2  // Left rear enable pin
@@ -14,7 +32,90 @@ const int LF_DIR[] = {33, 25}; // Left front direction pins
 
 unsigned long timer_reset;
 
+//---functions for getting notifications---
+static void notifyMusic(BLERemoteCharacteristic* pBLERemoteMusicChar, uint8_t* data, size_t length, bool isNotify)
+{
+  memcpy(&music_command, data, sizeof(music_command));
+  if(music_command == 17)
+    go = 1;
+}
+
+class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks  //when found a server
+{
+  void onResult(BLEAdvertisedDevice advertisedDevice)
+  {
+    if(advertisedDevice.getName() == bleServerName)  //check if the server is the right one
+    {
+      advertisedDevice.getScan()->stop();  //stop scanning
+      serverAddress = new BLEAddress(advertisedDevice.getAddress());  //get the server's address
+      serverFound = true;
+      Serial.println(" + Device found. Connecting...");
+    }
+  }
+};
+//---connect to the BLE server---
+bool connectToServer(BLEAddress address)
+{
+  Serial.println("-+-Creating BLE client...");
+  BLEClient* bleClient = BLEDevice::createClient();
+  
+  Serial.println("-+-Attempting connection...");
+  if(!bleClient->connect(address))
+  {
+    Serial.println("-x-Connection failed!");
+    return false;
+  }
+  Serial.println("-+-Connected to server, searching for services...");
+  delay(500);  //time for client to initialize
+  //find service
+  BLERemoteService* remoteService = nullptr;
+  remoteService = bleClient->getService(serviceUUID);
+  if(remoteService == nullptr)
+  {
+    Serial.println("-x-Failed to find service UUID!");
+    return false;
+  }
+  Serial.println("-+-Found service!");
+  //find characteristic
+  music_char = remoteService->getCharacteristic(musicCharUUID);
+  if(music_char == nullptr)
+  {
+    Serial.println("-x-Failed to find music characteristic UUID!");
+    return false;
+  }
+  Serial.println("-+-Found music characteristic!");
+  //turn notifications on
+  music_char->registerForNotify(notifyMusic);
+  music_char->getDescriptor(BLEUUID((uint16_t)0x2902))->writeValue((uint8_t*)notificationOn, 2, true);
+  return true;
+}
+
 void setup() {
+  //restart BLE and flash its memory
+  BLEDevice::deinit(true);
+  delay(500);
+  BLEDevice::init("");
+
+  //scan for servers
+  BLEScan* pBLEScan = BLEDevice::getScan();
+  pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
+  pBLEScan->setActiveScan(true);
+  pBLEScan->start(30);
+  while(!serverFound) delay(20);  //wait till server is found
+
+  if(connectToServer(*serverAddress))
+  {
+    Serial.println("-+-Successfully connected to BLE Server!");
+    connected = true;
+    serverFound = false;
+  }
+  else
+  {
+    Serial.println("-x-Connection failed! Restarting...");
+    delay(500);
+    ESP.restart();
+  }
+
   rd.attach(13);
   rv.attach(12);
   lv.attach(23);
@@ -36,6 +137,7 @@ void setup() {
 }
 
 void loop() {
+  while(!go) delay(10);
     timer_reset = millis();
     while ((millis() - timer_reset) < 5000) {
        forward(255);  
@@ -208,6 +310,8 @@ void loop() {
     backward(255);
     delay(4000);
     stop();
+
+    go = 0;
 }
 
 
