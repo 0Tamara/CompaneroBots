5863361a8900bfd7e954a2c6806ddfb841d6be64
martin-gondek <martingondekjuniorik@gmail.com>
2025-05-22 17:22:53 +0200

    debugging

--- pianist/lava_ruka.ino
+++ pianist/lava_ruka.ino
@@ -2,7 +2,7 @@
 
 #define numServos 8
 #define rezerva 100
-#define stepsPerNote 25;
+#define stepsPerNote 25
 #define stepsPerOctave 200
 unsigned long lastTime;
 
@@ -15,8 +15,6 @@ int leftServoPins[numServos] = { 2, 3, 4, 5, 6, 7, 8, 9 };
 //krok motor
 int stepPin = 10;
 int dirPin = 11;
-
-unsigned long lastTime;
 // casy
 int time = 1000;
 int osm = time / 8;
@@ -32,10 +30,25 @@ void setup() {
   pinMode(dirPin, OUTPUT);
 }
 
+void moveToOctave(int targetOctave) {
+  int direction = targetOctave - currentOctave;
+  if (direction == 0) return;
 
+  digitalWrite(dirPin, direction > 0 ? HIGH : LOW);
+  int steps = abs(direction) * stepsPerOctave;
+
+  for (int i = 0; i < steps; i++) {
+    digitalWrite(stepPin, HIGH);
+    delayMicroseconds(800);
+    digitalWrite(stepPin, LOW);
+    delayMicroseconds(800);
+  }
+
+  currentOctave = targetOctave;
+}
 
 void moveToNote(int targetNote, int targetOctave) {
-  int direction = (targetOctave * stepsPerOctave + targetNote * stepsPerNote) - (curretOctave * stepsPerOctave + currentNote * stepsPerNote);
+  int direction = (targetOctave * stepsPerOctave + targetNote * stepsPerNote) - (currentOctave * stepsPerOctave + currentNote * stepsPerNote);
 
   if (direction == 0) return;
 
@@ -49,42 +62,84 @@ void moveToNote(int targetNote, int targetOctave) {
     delayMicroseconds(800);
   }
 
-  currentOctave = octave;
+  currentOctave = targetOctave;
+  currentNote = targetNote;
 }
 
 //tu mozem spravit aj to, ze bude rovnaky kod ako na pravu ruku, ale vlastne mal by som noteIndex1 - 8, a posuval by som sa priamo na notu
-void playDualNote(int noteIndex1 int octave, int wait) {
-  moveToNote(noteIndex1, octave);
+void playDualNote(int note, int octave, int wait) {
+  moveToNote(note, octave);
   lastTime = millis();
   while (millis() - lastTime <= (wait - rezerva)) {
-    servos[noteIndex1].write(90);
-    servos[noteIndex2].write(90);
+    servos[0].write(90);
+    servos[7].write(90);
   }
   lastTime = millis();
   while (millis() - lastTime <= rezerva) {
-    servos[noteIndex1].write(0);
-    servos[noteIndex2].write(0);
+    servos[0].write(0);
+    servos[7].write(0);
   }
 }
-void playMelody() {
-  int melody[][3] = {
-    { 1, 1, stv },
-    { 5, 0, osm },
-    { 4, 1, pol },
-    { 1, 2, pol },
-    { 7, 0, osm } 
-  };
-
-  int length = sizeof(melody) / sizeof(melody[0]);
-
+void playNote(int noteIndex, int octave, int wait) {
+  moveToOctave(octave);
+  lastTime = millis();
+  while (millis() - lastTime <= (wait - rezerva)) {
+    servos[noteIndex].write(90);
+  }
+  lastTime = millis();
+  while (millis() - lastTime <= rezerva) {
+    servos[noteIndex].write(0);
+  }
+}
+void playDualMelody(int dualMelody[][3], int length) {
   for (int i = 0; i < length; i++) {
-    int note = melody[i][0];
-    int octave = melody[i][1];
-    int wait = melody[i][2];
+    int octave = dualMelody[i][0];
+    int wait = dualMelody[i][1];
+    int note = dualMelody[i][2];
     playDualNote(note, octave, wait);
   }
 }
 
+void playMelody(int melody[][3], int length) {
+  for (int i = 0; i < length; i++) {
+    int octave = melody[i][0];
+    int wait = melody[i][1];
+    int note = melody[i][2];
+    playNote(note, octave, wait);
+  }
+}
+int dualMelody1[][3] = {
+    { 1, stv, 2 },
+    { 5, osm, 3 },
+    { 1, stv, 4 },
+    { 5, osm, 5 },
+    { 1, stv, 6 },
+    { 5, osm, 7 },
+    { 1, stv, 5 },
+    { 5, osm, 4 }
+};
+
+int melody1[][3] = {
+  //h
+    { 1, stv, 0 },
+    { 1, osm, 1 },
+    { 1, stv, 2 },
+    { 1, osm, 3 },
+    { 1, stv, 4 },
+    { 1, osm, 5 },
+    { 1, stv, 6 },
+    { 1, osm, 7 }
+  //d
+    { 1, stv, 7 },
+    { 1, osm, 6 },
+    { 1, stv, 5 },
+    { 1, osm, 4 },
+    { 1, stv, 3 },
+    { 1, osm, 2 },
+    { 1, stv, 1 },
+    { 1, osm, 0 }
+};
 void loop() {
-  playMelody();
+  playDualMelody(dualMelody1, sizeof(dualMelody1) / sizeof(dualMelody1[0]));
+  playMelody(melody1, sizeof(melody1) / sizeof(melody1[0]));
 }
