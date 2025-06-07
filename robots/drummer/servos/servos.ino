4890d508c08d32d8f6b2bdf0c0d3db7b30490159
tomas_cakan <cakant2008@gmail.com>
2025-06-07 14:34:06 +0200

    Revert "drummer_HW_working"
    This reverts commit fe3a571c54342204ccf31f23c1dc99ffba737764.

--- robots/drummer/servos/servos.ino
+++ robots/drummer/servos/servos.ino
@@ -1,24 +1,23 @@
 #include <ESP32Servo.h>
 
-#define R_ARM_PIN 13
-#define L_ARM_PIN 12
-#define KICK_PIN 33
+#define R_ARM_PIN 27
+//#define L_ARM_PIN 21
+//#define KICK_PIN 19
 
 Servo r_arm;  //0-80 = down-front
-Servo l_arm;  //80-0 = down-front
-Servo kick;   //85-90-85 = kick
+//Servo l_arm;  //80-0 = down-front
+//Servo kick;   //85-90-85 = kick
 
 void setup()
 {
   Serial.begin(115200);
 
   r_arm.attach(R_ARM_PIN);
-  l_arm.attach(L_ARM_PIN);
-  kick.attach(KICK_PIN);
+  //l_arm.attach(L_ARM_PIN);
+  //kick.attach(KICK_PIN);
 }
 
 void loop()
 {
-  r_arm.write(80);
-  l_arm.write(0);
+  r_arm.write(180);
 }
