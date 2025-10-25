d0afdddee07dac235df2ab691fb9331fdd5fe244
tomas_cakan <cakant2008@gmail.com>
2025-10-25 14:18:16 +0200

    LEDs working

--- exhibition/drummer/universal_song_reading/src/main.cpp
+++ exhibition/drummer/universal_song_reading/src/main.cpp
@@ -12,7 +12,7 @@
 #define KICK_SERVO_PIN 32
 #define LED_PIN_LEFT 27
 #define LED_PIN_RIGHT 14
-#define LED_PIN_KKICK 33
+#define LED_PIN_KICK 33
 #define LED_PIN_EYES 23
 //-values-
 #define LEFT_SERVO_RELEASE 20
@@ -44,6 +44,21 @@ size_t header_lines[MAX_SONGS] = {0};  //the first line of each song
 size_t csv_pointer = 0;
 File database;
 
+//--leds--
+uint color_eyes = 0x800080;
+uint color_palette[6] = {0x800000,  //colors cycling over
+                        0x404000,
+                        0x008000,
+                        0x004040,
+                        0x000080,
+                        0x400040};
+int rising_color[3] = {0, 0, 0};  //color used for rising effects (during music)
+bool blink_drums[3] = {0, 0, 0};  //rising effects active
+int LEDs_pos[3] = {0, 0, SIDES_LED_COUNT-1};  //position on the LED ring
+bool rising[3] = {1, 1, 1};  //rising / lowering
+bool miss_out[3] = {0, 0, 0};  //missing out every other step to go slower
+TaskHandle_t Task1;
+
 struct song
 {
   String name;
@@ -61,6 +76,17 @@ struct bar_type
   uint16_t right_notes;
 };
 bar_type current_bar;
+
+//---change colors when the drum gets hit---
+void changeColors(int drum/*0-left; 1-kick; 2-right*/)
+{
+  if(rising_color[drum] < 5)
+    rising_color[drum]++;
+  else
+    rising_color[drum] = 0;
+  rising[drum] = 1;
+  blink_drums[drum] = 1;
+}
 
 void loadDatabase()  //read the file and save song starting lines
 {
@@ -186,18 +212,126 @@ void playBar()
   for (int i = current_song.notes_per_bar-1; i >= 0; i--)
   {
     if ((current_bar.left_notes & 1<<i) > 0)
+    {
       left_servo.write(LEFT_SERVO_HIT);
+      changeColors(0);
+    }
     if ((current_bar.kick_notes & 1<<i) > 0)
+    {
       kick_servo.write(KICK_SERVO_HIT);
+      changeColors(1);
+    }
     if ((current_bar.right_notes & 1<<i) > 0)
+    {
       right_servo.write(RIGHT_SERVO_HIT);
-    
+      changeColors(2);
+    }
+
     delay(100);
     //-release keys-
     left_servo.write(LEFT_SERVO_RELEASE);
     kick_servo.write(KICK_SERVO_RELEASE);
     right_servo.write(RIGHT_SERVO_RELEASE);
     while (millis() - timer_bar <= current_song.note_length * (current_song.notes_per_bar - i));
+  }
+}
+
+//---loop for LED effects (during music)---
+void loop_2(void* parameter)
+{
+  //---loop2---
+  while(true)
+  {
+    if(blink_drums[0])  //left effect active
+    {
+      if(rising[0])  //lighting up
+      {
+        left_LEDs[LEDs_pos[0]] = color_palette[rising_color[0]];
+        LEDs_pos[0] ++;
+
+        if(LEDs_pos[0] == SIDES_LED_COUNT)  //prepare values for turn off stage
+        {
+          rising[0] = 0;
+          LEDs_pos[0] = SIDES_LED_COUNT-1;
+        }
+      }
+      else  //turning off
+      {
+        left_LEDs[LEDs_pos[0]] = 0;
+        LEDs_pos[0] --;
+
+        if(LEDs_pos[0] < 0)  //revert values
+        {
+          rising[0] = 1;
+          blink_drums[0] = 0;
+          LEDs_pos[0] = 0;
+          miss_out[0] = 0;
+        }
+      }
+    }
+
+    if(blink_drums[1])  //kick effect active
+    {
+      if(rising[1])  //lighting up
+      {
+        kick_LEDs[LEDs_pos[1]] = color_palette[rising_color[1]];
+        kick_LEDs[LEDs_pos[1]+1] = color_palette[rising_color[1]];
+        kick_LEDs[(KICK_LED_COUNT-1)-LEDs_pos[1]] = color_palette[rising_color[1]];  //going from both sides to middle
+        kick_LEDs[(KICK_LED_COUNT-1)-LEDs_pos[1]-1] = color_palette[rising_color[1]];
+        LEDs_pos[1] +=2;
+
+        if(LEDs_pos[1] > KICK_LED_COUNT/2-1)  //prepare values for turn off stage
+        {
+          rising[1] = 0;
+          LEDs_pos[1] = KICK_LED_COUNT/2-1;
+        }
+      }
+      else  //turning off
+      {
+        kick_LEDs[LEDs_pos[1]] = 0;
+        kick_LEDs[(KICK_LED_COUNT-1)-LEDs_pos[1]] = 0;
+        LEDs_pos[1] --;
+
+        if(LEDs_pos[1] < 0)  //revert values
+        {
+          rising[1] = 1;
+          blink_drums[1] = 0;
+          LEDs_pos[1] = 0;
+          miss_out[1] = 0;
+        }
+      }
+    }
+
+    if(blink_drums[2])  //right effect active
+    {
+      if(rising[2])
+      {
+        right_LEDs[LEDs_pos[2]] = color_palette[rising_color[2]];
+        LEDs_pos[2] --;
+
+        if(LEDs_pos[2] < 0)  //prepare values for turn off stage
+        {
+          rising[2] = 0;
+          LEDs_pos[2] = 0;
+        }
+      }
+      else
+      {
+        right_LEDs[LEDs_pos[2]] = 0;
+        LEDs_pos[2] ++;
+
+        if(LEDs_pos[2] == SIDES_LED_COUNT)  //revert values
+        {
+          rising[2] = 1;
+          blink_drums[2] = 0;
+          LEDs_pos[2] = SIDES_LED_COUNT-1;
+          miss_out[2] = 0;
+        }
+      }
+    }
+    
+    FastLED.show();
+    delay(10);
   }
 }
 
@@ -222,10 +356,55 @@ void setup()
   kick_servo.write(KICK_SERVO_RELEASE);
   right_servo.write(RIGHT_SERVO_RELEASE);
   left_servo.write(LEFT_SERVO_RELEASE);
+
+  //-create loop 2-
+  xTaskCreatePinnedToCore(
+      loop_2, /* Function to implement the task */
+      "Task1", /* Name of the task */
+      10000,  /* Stack size in words */
+      NULL,  /* Task input parameter */
+      0,  /* Priority of the task */
+      &Task1,  /* Task handle. */
+      0); /* Core where the task should run */
+
+  //-init LEDs and blink white-
+  FastLED.addLeds<WS2811, LED_PIN_LEFT, GRB>(left_LEDs, SIDES_LED_COUNT);
+  FastLED.addLeds<WS2811, LED_PIN_KICK, GRB>(kick_LEDs, KICK_LED_COUNT);
+  FastLED.addLeds<WS2811, LED_PIN_RIGHT, GRB>(right_LEDs, SIDES_LED_COUNT);
+  FastLED.addLeds<WS2811, LED_PIN_EYES, GRB>(eyes_LEDs, EYES_LED_COUNT);
+
+  for (int i = 0; i < 54; i++) {
+    if (i < SIDES_LED_COUNT) right_LEDs[i] = 0x808080;
+    if (i < SIDES_LED_COUNT) left_LEDs[i] = 0x808080;
+    if (i < KICK_LED_COUNT) kick_LEDs[i] = 0x808080;
+  }
+  FastLED.show();
+
+  delay(100);
+  for (int i = 0; i < 54; i++) {
+    if (i < SIDES_LED_COUNT) right_LEDs[i] = 0x000000;
+    if (i < SIDES_LED_COUNT) left_LEDs[i] = 0x000000;
+    if (i < KICK_LED_COUNT) kick_LEDs[i] = 0x000000;
+  }
+  FastLED.show();
 }
 
 void loop()
 {
+  loadSongHeader(1);
+  current_bar.end = 0;
+  timer_general = millis();
+  for(int i=0; i<current_song.length; i++)
+  {
+    if(i == current_bar.end)
+      loadBar();
+    playBar();
+    Serial.printf("Played bar ending at %d; t= %d bars\n", current_bar.end, i);
+    while(millis() - timer_general < current_song.note_length * current_song.notes_per_bar);
+    timer_general = millis();
+  }
+  delay(3000);
+
   loadSongHeader(0);
   current_bar.end = 0;
   timer_general = millis();
